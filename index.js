a48ca3b479c8290ef30c4338a2b942e96e14390b
vivaxy <xyxuye2007@126.com>
2016-11-16 14:06:07 +0800

    fix babel-register.ignore

--- index.js
+++ index.js
@@ -6,15 +6,25 @@
  */
 
 const path = require('path');
+const minimatch = require('minimatch');
 
 require('babel-polyfill');
 // http://stackoverflow.com/questions/35120305/using-babel-register-in-my-cli-npm-app-works-locally-but-not-globally-after-pub/35120765#35120765
 require('babel-register')({
     ignore: (filename) => {
-        const ignorePath = path.join(__dirname, `node_modules`);
-        const ignored = filename.indexOf(ignorePath) > -1;
-        // console.log(filename, ignored);
-        return ignored;
+        // ignore **/node_modules/granturismo/node_modules/**
+        // not ignore **/node_modules/granturismo/**
+        // ignore **/node_modules/**
+        if (minimatch(filename, `**/node_modules/granturismo/node_modules/**`)) {
+            return true;
+        }
+        if (minimatch(filename, `**/node_modules/granturismo/**`)) {
+            return false;
+        }
+        if (minimatch(filename, `**/node_modules/**`)) {
+            return true;
+        }
+        return false;
     },
 });
 require('./lib/main');
